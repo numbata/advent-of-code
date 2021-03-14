43d44e897d395a97455ca9381183d3aab12d3b4b
Andrei Subbota <subbota@gmail.com>
2021-03-14 22:37:48 +0200

    Add y2020/d01/p1

---
+++ 2020/day-01/part-1.rb
@@ -0,0 +1,21 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require 'set'
+
+input_fname = ARGV[0] || 'input-01.txt'
+input = File.open(input_fname, 'r')
+
+accum = Set.new
+
+input.each_line do |line|
+  entry = line.chomp.to_i
+  expect = 2020 - entry
+
+  if accum.include?(expect)
+    puts entry * expect
+    exit 0
+  end
+
+  accum << entry
+end
